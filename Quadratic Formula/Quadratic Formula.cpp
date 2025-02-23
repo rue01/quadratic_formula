27fd5f58d223c3e9a020218434c894e492677219
mulundi01 <18102315+rue01@users.noreply.github.com>
2025-02-23 23:56:07 +0000

    Remove commented code

--- Quadratic Formula/Quadratic Formula.cpp
+++ Quadratic Formula/Quadratic Formula.cpp
@@ -1,12 +1,4 @@
 // Quadratic Formula.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-//#include <iostream>
-//
-//int main()
-//{
-//    std::cout << "Hello World!\n";
-//}
 
 #include <iostream>
 #include <cmath>
